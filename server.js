dd42d22aa44f38e8413f163b6ba17f5a54c733e6
D-Know <n.dino90@gmail.com>
2020-08-24 15:38:48 -0600

    everything works except continueQuit

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ funcsObject = {
         }
       });
 
-      console.table(departments);
-    }).then(continueQuit())
+      console.table(departments)
+      
+    })
+    
     // ;
   },
 
@@ -66,13 +68,13 @@ funcsObject = {
     inquirer.prompt([
       {
         type: 'input',
-        name: 'role',
+        name: 'department',
         message: 'Name of new department'
       }
     ]).then(ans=>{
       console.log(ans.role)
       //variable to pass over necessary add information
-    const deptName = ans.role;
+    const deptName = ans.department;
     module.exports = deptName;
     database.addDepartment(deptName).then
     funcsObject['viewAllDepartments']()
@@ -82,22 +84,123 @@ funcsObject = {
   },
 
   addRole: () => {
-  
-    //variable to pass over necessary add information
-    const roleData = '("Sales Secretary", 45000, 3)';
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Name of new role'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'How much does this role pay'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'How much does this role pay (no commas)'
+
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: "Which department does this role belong to?",
+        choices: [
+                  {
+                    name: "IT",
+                    value: 1},
+                  {
+                    name: "Procurement",
+                    value: 2
+                  },
+                  {
+                    name: "Sales",
+                    value: 3
+                  }
+                  ]
+      }
+    ]).then(ans=>{
+    const roleData = `("${ans.role}", ${ans.salary}, ${ans.department})`;
     console.log(roleData)
     //pass data to index.js
     module.exports = roleData;
     database.addNewRole(roleData).then
-    getRoles();
+    funcsObject['viewAllRoles']();
+
+    })
+    
+   
   },
 
   addEmployee: () => {
-    //variable to pass over 
-    const employeeData = '("Eugene", "Sampson", 10, "Dick Cooke")'
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'First Name of employee'
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'Last Name of employee'
+      },
+      {
+        type: 'list',
+        name: 'job',
+        message: 'What is this employees role',
+        choices: [
+           
+          {
+            name: "Tech Support", 
+            value: [3, '"Adan McCormick"']
+          },
+          {
+            name: "Junior Developer",
+            value: [4, '"Adan McCormick"']
+          },
+          {
+            name: "Senior Developer",
+            value: [5, '"Adan McCormick"']
+          },
+          {
+            name: "Junion Buyer",
+            value: [7, '"Clair Carter"']
+          },
+          {
+            name: "Senior Buyer",
+            value: [8, '"Clair Carter"']
+          },
+          {
+            name: "Sales Support",
+            value: [10, '"Dick Cooke"']
+          },
+          {
+            name: "Account Manager",
+            value: [11, '"Dick Cooke"']
+          },
+          {
+            name: "Sales Executive",
+            value: [12, '"Dick Cooke"']
+          }
+        ]
+
+      }//,
+      // {
+      //   type: 'list',
+      //   name: 'manager',
+      //   message: "Who is their manager",
+      //   choices: ["Adan McCormick", "Clair Carter", "Dick Cooke"]
+      // }
+    ]).then(ans =>{
+       //variable to pass over 
+    const employeeData = `("${ans.firstName}", "${ans.lastName}", ${ans.job})`
+    console.log(employeeData)
     module.exports = employeeData;
     database.addNewEmployee(employeeData).then
-    getEmployees();
+    funcsObject['viewAllEmployees']()
+
+    })
+   ;
 
   },
 
@@ -106,7 +209,8 @@ funcsObject = {
     // figure out how to exit npm
     process.exit
   }
-};
+  
+}
 
 //start of application
 
@@ -164,6 +268,7 @@ const promptPortal = () => {
       //call the function to viw
       funcsObject[answer.menu]()
       
+      
      
 
     } else {
@@ -173,10 +278,30 @@ const promptPortal = () => {
     
     }
 
+    
+ /*  originally had planned on trying to keep my code concise and
+       taking the selection, turining it camelcase and then 
+       using that to call function.  Got on help BCS and explained
+      that it wouldnt work because the code below would return a string.  
+      katheryn from BCS explained i could use object methods to accomplish 
+      this
+ 
+  .then(answer =>{
+    //make string have first letter uppercase
+    let choice = answer.menu
+    const selection = choice.toLowerCase()
+      .split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join('')
+      
+      //need to lowercase first letter
+      const camelCase = selection.charAt(0).toLowerCase() + selection.slice(1);
+   */
+
   })
  
 }
-const continueQuit = () =>{
+const continueQuit = ()=> {
   inquirer.prompt({
     name: 'continue',
     type: 'confirm',
@@ -194,4 +319,5 @@ const continueQuit = () =>{
 }
 
 
-promptPortal();
+promptPortal()
+  
